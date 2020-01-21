a753a811548f0562e7ef40ca2b42bf3dff37c14b
Aindrila Dhar <aindriladhar@gmail.com>
2020-01-21 16:31:53 +0530

    DAY 2 commit

--- app/src/main/java/com/example/jandhandarshak/registration.java
+++ app/src/main/java/com/example/jandhandarshak/registration.java
@@ -1,36 +1,56 @@
 package com.example.jandhandarshak;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 public class registration extends AppCompatActivity {
+    private Button b1;
+    String bankvalue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         Spinner spinner = findViewById(R.id.noOfbanks);
         ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Select No of Banks");
         arrayList.add("1");
         arrayList.add("2");
         arrayList.add("3");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,                         android.R.layout.simple_spinner_item, arrayList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arrayList);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String tutorialsName = parent.getItemAtPosition(position).toString();
-                //Toast.makeText(parent.getContext(), "Selected: " + tutorialsName,          Toast.LENGTH_LONG).show();
+                bankvalue = parent.getItemAtPosition(position).toString();
+              //  Toast.makeText(parent.getContext(), "Selected: " + bankvalue,          Toast.LENGTH_LONG).show();
             }
             @Override
             public void onNothingSelected(AdapterView <?> parent) {
             }
         });
+
+        b1=(Button) findViewById(R.id.nextPage);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v1) {
+
+               Intent intent = new Intent(registration.this,bankform.class);
+               intent.putExtra("bankVal",bankvalue);
+               startActivity(intent);
+
+           }
+        });
+
     }
+
 }
